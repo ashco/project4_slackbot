5e941cc34a1f57b995acf7998bf883aa38be168a
Ashton <ashchristie@gmail.com>
2018-02-22 00:43:33 -0800

    bot should be able to repeat what is sent to it

--- slackbot.py
+++ slackbot.py
@@ -1,35 +1,62 @@
 from slackclient import SlackClient
 import time
 
-slack_client = SlackClient('xoxb-318344157728-aaRHDSUOlWxo9utrM2Ziej0e')
-# print(help(slack_client))
+class slackCommunication(object):
+    def __init__(self):
+        self.slack_client = SlackClient('xoxb-318344157728-aaRHDSUOlWxo9utrM2Ziej0e')
+        self.appName = 'super_bot'
+    # print(help(slack_client))
+    def slackConnect(self):
+        return self.slack_client.rtm_connect()
 
-def slackConnect():
-    return slack_client.rtm_connect()
+    # REAL TIME MESSAGE READER FNC
+    def slackReadRTM(self):
+        while True: # While connected, print off connection info
+            print(self.slack_client.rtm_read()) 
+            time.sleep(1) # in 1 second intervals
 
-# REAL TIME MESSAGE READER FNC
-def slackReadRTM():
-    while True: # While connected, print off connection info
-        print(slack_client.rtm_read()) 
-        time.sleep(1) # in 1 second intervals
+    def parseSlackInput(self, input, botID):
+        botATID = '<@' + botID + '>'
+        if input and len(input) > 0: #if there is something inside input
+            input = input[0]
+            if 'text' in input and botATID in input['text']:
+                user = input['user']
+                message = input['text'].split(botATID)[1].strip(' ')
+                channel = input['channel']
+                return [user, message, channel]
+            else: 
+                return [None, None, None]
 
-def parseSlackInput(input, botID):
-    botATID = '<@' + botID + '>'
-    if input and len(input) > 0: #if there is something inside input
-        input = input[0]
-        if 'text' in input and botATID in input['text']:
-            user = input['user']
-            message = input['text'].split(botATID)[1].strip(' ')
-            channel = input['channel']
-            return [user, message, channel]
-        else: 
-            return [None, None, None]
+    def getBotID(self, bot_username):
+        api_call = self.slack_client.api_call('users.list')
+        users = api_call['members']
+        for user in users: 
+            if 'name' in user and bot_username in user.get('name') and not user.get('deleted'):
+                return user.get('id')
 
-def getBotID(bot_username):
-    api_call = slack_client.api_call('users.list')
-    users = api_call['members']
-    for user in users: 
-        if 'name' in user and bot_username in user.get('name') and not user.get('deleted'):
-            return user.get('id')
+    def writeToSlack(self, channel, message):
+        return self.slack_client.api_call('chat.postMessage', channel = channel, text = message, as_user = True)
 
-# print(getBotID('superbot'))
+
+class mainFunc(slackCommunication):
+    def __init__(self):
+        super(mainFunc, self).__init__()
+        # BOTID = self.getBotID(self.appName)
+
+    def descideWhetherToTakeAction(self, input):
+        if input:
+            user, message, channel = input
+            return self.writeToSlack(channel, message)
+            # Can run functio that does anything here
+
+    def run(self):
+        self.slackConnect()
+        BOTID = self.getBotID(self.appName)
+        while True:
+            self.descideWhetherToTakeAction(self.parseSlackInput(self.slackReadRTM(),BOTID))
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    instance = mainFunc()
+    instance.run()
--- test_slackbot.py
+++ test_slackbot.py
@@ -1,19 +1,42 @@
 import pytest
-from slackbot import * #import main program into test
+# from slackbot import * #import main program into test
 
 input = [{'type': 'message', 'channel': 'C9D022F8A', 'user': 'U9D7A90VD', 'text': '<@U9CA44MME> TEST01', 'ts': '1519284735.000022', 'source_team': 'T9BJYGP41', 'team': 'T9BJYGP41'}]
 
+@pytest.fixture # import class to test
+def slackCommunication():
+    from slackbot import slackCommunication #import class from document
+    return slackCommunication() # instantiate class in testfile
+
+@pytest.fixture
+def mainFunc():
+    from slackbot import mainFunc
+    return mainFunc()
+
+
 # @pytest.mark.skip(reason='fully tested')
-def test_slackConnect():
-    assert slackConnect() == True
+def test_slackConnect(slackCommunication):
+    assert slackCommunication.slackConnect() == True
 
-@pytest.mark.skip(reason="not fully implemented")
-def test_slackReadRTM():
-    print(slackReadRTM())
-
-def test_parseSlackInput():
-    parseSlackInput(input, 'U9CA44MME') == ['U9D7A90VD', 'test01', 'C9D022F8A'] # User / text / channel
+def test_parseSlackInput(slackCommunication):
+    slackCommunication.parseSlackInput(input, 'U9CA44MME') == ['U9D7A90VD', 'test01', 'C9D022F8A'] # User / text / channel
 
 # @pytest.mark.skip(reason='wip')
-def test_getBotID():
-    assert getBotID('super_bot') == 'U9CA44MME'
+def test_getBotID(slackCommunication):
+    assert slackCommunication.getBotID('super_bot') == 'U9CA44MME'
+
+def test_writeToSlack(slackCommunication):
+    assert slackCommunication.writeToSlack('C9D022F8A', 'TEST: Writing to Slack')['ok'] == True # channel, message
+
+@pytest.mark.skip(reason="not fully implemented")
+def test_slackReadRTM(slackCommunication):
+    slackCommunication.slackConnect()
+    print(slackCommunication.slackReadRTM())
+
+def test_descideWhetherToTakeAction_Message(mainFunc):
+    input = ['U9D7A90VD', 'test02', 'C9D022F8A'] # User / text / channel
+    assert mainFunc.descideWhetherToTakeAction(input)
+
+def test_descideWhetherToTakeAction_None(mainFunc):
+    input = [None, None, None] # User / text / channel
+    assert mainFunc.descideWhetherToTakeAction(input)
